51e69ded7ab6d8ce73d5ff19fb8a2a5974fb4f4c
shyam9493 <shyamnandapuneedi123@gmail.com>
2024-04-27 01:15:13 +0530

    added required changes

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php
-$api_key = 'c9fc4d0d109d4a34a52183941242604';
-$location = 'New York'; // or any other location
+$api_key = 'dd19dca0dc4949c6a4c191534242604';
+$location = 'India';
 
-$url = "https://api.weather.com/v3/weather/now?apiKey=$api_key&location=$location";
+$url = "http://api.weatherapi.com/v1/current.json?key=$api_key&q=$location&aqi=no";
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
-curl_close($ch);
+
+if ($response === false) {
+    die('Error: Failed to retrieve weather data.');
+}
+
 
 $weather_data = json_decode($response, true);
-$weather_summary = $weather_data['forecast']['summary']['description'];
-echo "Weather Summary: $weather_summary";
+
+if (isset($weather_data['current']['temp_c'])){
+    $temperature_celsius = $weather_data['current']['temp_c'];
+    $temperature_fahrenheit = $weather_data['current']['temp_f'];
+    $humidity = $weather_data['current']['humidity'];
+    $precipitation_mm = $weather_data['current']['precip_mm'];
+    $weather_condition = $weather_data['current']['condition']['text'];
+
+    echo "Temperature:",$temperature_celsius,"<br>";
+    echo "Humidity: $humidity <br>";
+    echo $precipitation_mm,"<br>";
+    echo $weather_condition;
+
+}
+
+curl_close($ch);
 ?>
